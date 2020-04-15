6698c11a8e0c16e1ab023a1b3836a1882d4712e9
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-15 18:12:37 +0700

    JUnit 5: Step 03 - Absence of failure is success

---
+++ src/test/java/StringTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class StringTest {
+
+    @Test
+    void whatEverYouWant()
+    {
+        int actualLength = "ABCD".length();
+
+        int expectedLength = 4;
+
+        assertEquals(expectedLength, actualLength);
+
+
+        // Write test code
+        // Invoke method square(4) => CUT
+        // Checks in place - 16 => Assertions
+    }
+}
