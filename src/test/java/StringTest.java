b62f53e74e906eb4e7e6e376bfbd18a42af43c1e
BOONKUAE BOONSUTTA <boonkuae.boonsutta@scb.co.th>
2020-04-15 18:40:41 +0700

    JUnit 5: Step 06 - Writing Assertions for Arrays - assertArrayEquals

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -45,4 +45,24 @@ public class StringTest {
         assertEquals(true,result);
         // assertTrue(result);
     }
+    @Test
+    void slim_contain_basic()
+    {
+        assertFalse("abcdefghijk".contains("xyz"));
+    }
+
+    @Test
+    void split_basic()
+    {
+        // Given
+        String str = "abc def ghi";
+
+        // When
+        String actualResult[] = str.split(" ");
+
+        // Assertion
+        assertArrayEquals(new String[] {"abc", "def", "ghi"}, actualResult);
+//        String expectResult[] = new String[] {"abc", "def", "ghi"};
+//        assertArrayEquals(expectResult, actualResult);
+    }
 }
